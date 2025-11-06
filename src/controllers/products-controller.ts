8cfa270875a6eb17cc997b59c22b7fbdc35c03c7
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-11-06 12:14:57 -0300

    feat: add product at database

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from "express";
+import { knex } from "../database/knex";
+import z from "zod";
 
 class ProductsController {
   async create(req: Request, res: Response) {
-    res.status(201).json({ message: "Product created" });
+    try {
+      const bodySchema = z.object({
+        name: z
+          .string()
+          .min(4, "O nome do produto deve ter pelo menos 4 caracteres"),
+        price: z.number().positive(),
+      });
+
+      const { name, price } = bodySchema.parse(req.body);
+
+      await knex<ProductRepository>("products").insert({ name, price });
+
+      return res.status(201).json();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json(error.issues);
+      }
+    }
   }
 }
 
