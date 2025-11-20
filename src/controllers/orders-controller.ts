907cddd86b2552d4d74a23b507d662c9c2b647fe
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-11-20 20:48:25 -0300

    feat: create and list orders

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,41 +1,81 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import z from "zod";
 import { knex } from "@/database/knex";
 import { AppError } from "@/utils/AppError";
 
 class OrdersController {
-  async create(req: Request, res: Response) {
-    const bodySchema = z.object({
-      table_session_id: z.number(),
-      product_id: z.number(),
-      quantity: z.number(),
-    });
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_session_id: z.number(),
+        product_id: z.number(),
+        quantity: z.number(),
+      });
 
-    const { table_session_id, product_id, quantity } = bodySchema.parse(
-      req.body
-    );
+      const { table_session_id, product_id, quantity } = bodySchema.parse(
+        req.body
+      );
 
-    const session = await knex<TablesSessionsRepository>("tables_sessions")
-      .where({ id: table_session_id })
-      .first();
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first();
 
-    if (!session) {
-      throw new AppError("Session not found", 404);
+      if (!session) {
+        throw new AppError("Session not found", 404);
+      }
+
+      if (session.closed_at) {
+        throw new AppError("Session closed", 400);
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first();
+
+      if (!product) {
+        throw new AppError("Product not found", 404);
+      }
+
+      await knex<OrderRepository>("orders").insert({
+        table_session_id,
+        product_id,
+        quantity,
+        price: product.price,
+      });
+
+      return res.status(201).json();
+    } catch (error) {
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+      }
+      next(error);
     }
+  }
 
-    if (session.closed_at) {
-      throw new AppError("Session closed", 400);
+  async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      const table_session_id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(req.params.table_session_id);
+
+      const orders = await knex<OrderRepository>("orders")
+        .where({ table_session_id })
+        .select(
+          "orders.id",
+          "orders.table_session_id",
+          "orders.product_id",
+          "orders.price",
+          "orders.quantity",
+          "products.name"
+        )
+        .join("products", "products.id", "orders.product_id");
+
+      return res.status(200).json(orders);
+    } catch (error) {
+      next(error);
     }
-
-    const product = await knex<ProductRepository>("products")
-      .where({ id: product_id })
-      .first();
-
-    if (!product) {
-      throw new AppError("Product not found", 404);
-    }
-
-    return res.status(201).json(product);
   }
 }
 
--- src/routes/orders-router.ts
+++ src/routes/orders-router.ts
@@ -5,5 +5,6 @@ const ordersRouter = Router();
 const ordersController = new OrdersController();
 
 ordersRouter.post("/", ordersController.create);
+ordersRouter.get("/table-session/:table_session_id", ordersController.index);
 
 export { ordersRouter };
